d29d1569f6ea37a083df1bf103cbe06ed20062cf
Galen-Dively <65838051+Galen-Dively@users.noreply.github.com>
2024-11-22 15:58:31 -0500

    Update register.php

--- register.php
+++ register.php
@@ -4,7 +4,7 @@ session_start();
 // Database connection
 $servername = "localhost";
 $username = "web_server";
-$password = "jZ5/=nFwAuMRm_y(9%dBU";
+$password = "";
 $dbname = "websiteDB";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
